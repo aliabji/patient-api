ed2a6e5637805d10159148fe48a08803ef0dfcf0
aliabji <aliabji@gmail.com>
2018-07-29 19:26:06 -0400

    patient updating successfully

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -19,7 +19,7 @@ class PatientsController < ApplicationController
     end
 
     def update
-        if @patient.update(patient_params)
+        if Patient.update(patient_params)
             render json: {status: 'SUCCESS', message: 'Patient Updated Sucessfully'}, status: :ok
         else
             render json: {status: 'FAILED', message: 'Patient Update Failed'}, status: :error
